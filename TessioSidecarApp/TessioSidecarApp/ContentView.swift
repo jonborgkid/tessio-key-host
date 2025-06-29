832fd89d703f92b90c1960b8349255e3f15052b1
Jonathan Borgia <jonathan.borgia@protonmail.com>
2025-06-28 21:01:31 -0600

    Pre-sync commit of staged changes

---
+++ TessioSidecarApp/TessioSidecarApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TessioSidecarApp
+//
+//  Created by Jonathan Borgia on 6/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TessioSidecarApp/TessioSidecarApp/TessioSidecarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TessioSidecarAppApp.swift
+//  TessioSidecarApp
+//
+//  Created by Jonathan Borgia on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TessioSidecarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
